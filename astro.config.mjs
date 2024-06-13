d522623817ebc6889e5ba18d90b66fdb2c323ad1
Keith Edyburn <keith@edyburn.info>
2024-06-13 18:51:21 +0000

    Install astro-icon.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    server: { host: true },
-    site: 'https://keith.edyburn.info',
+  server: { host: true },
+  site: "https://keith.edyburn.info",
+  integrations: [icon()],
 });
