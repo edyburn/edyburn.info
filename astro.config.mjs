4017c08e780f32bd483ef07bd13bca5df1782d8a
Keith Edyburn <keith@edyburn.info>
2024-06-13 14:45:58 +0000

    Expose astro dev server on non-localhost IPs.
    Dev container port forwarding expects the port to be "public" inside
    the container.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    server: { host: true },
     site: 'https://keith.edyburn.info',
 });
