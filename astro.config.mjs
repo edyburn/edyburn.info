e8a00060303e1c6592e1ccfd8f6ee0086341cf6d
Keith Edyburn <keith@edyburn.info>
2024-06-13 22:23:41 +0000

    Install remark-toc plugin.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
+import remarkToc from "remark-toc";
 
 // https://astro.build/config
 export default defineConfig({
   server: { host: true },
   site: "https://keith.edyburn.info",
   integrations: [icon()],
+  markdown: {
+    remarkPlugins: [remarkToc],
+  },
 });
