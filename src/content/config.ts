6edc51e17b6e352a4e6992b150dcf6b1e3a5d195
Keith Edyburn <keith@edyburn.info>
2024-06-14 04:46:13 +0000

    Configure content collection for jobs.

---
+++ src/content/config.ts
@@ -0,0 +1,30 @@
+import { defineCollection, z } from "astro:content";
+
+export const collections = {
+  jobs: defineCollection({
+    type: "content",
+    schema: z.object({
+      companyName: z.string(),
+      aka: z.string().optional(),
+      url: z.string().url().optional(),
+      note: z.string().optional(),
+      location: z.string(),
+      // This avoids finding the min start date of positions to sort jobs
+      initialStartDate: z.string().date(),
+      positions: z.array(
+        z.object({
+          title: z.string(),
+          // Omit for full-time
+          classification: z
+            .union([z.literal("part-time"), z.literal("freelance")])
+            .optional(),
+          // The "day" part of these dates are not used
+          startDate: z.string().date(),
+          endDate: z.union([z.string().date(), z.literal("present")]),
+        })
+      ),
+      // TODO: Add a brief description of the company?
+      // TODO: Add list of skills?
+    }),
+  }),
+};
